f1ea7b51b9958fce7d00d1dccd4f353fb5b0bf80
Nethsara Prabash <138272058+nethsaraPrabash@users.noreply.github.com>
2024-12-20 20:43:32 +0530

    hot fix excelsheet

--- src/pages/Excelsheet.tsx
+++ src/pages/Excelsheet.tsx
@@ -1,18 +1,32 @@
 import Spreadsheet from "react-spreadsheet";
-
+import "../styles/Excelsheet.css";
 
 export const ExcelSheet = () => {
-
-  const columnLabels = ["ID", "Name", ...Array.from({ length: 31 },(_, i) => (i+1).toString()) , "Count", "%", "Total"];
-
-  const data = [
-    [{ value: 1 }, { value: "John Doe" }, ...Array.from({ length: 31 }, () => ({ value: Math.floor(Math.random() * 100) })), { value: 0 }, { value: 0 }, { value: 0 }],
+  const columnLabels = [
+    "ID",
+    "Name",
+    ...Array.from({ length: 31 }, (_, i) => (i + 1).toString()),
+    "Count",
+    "%",
+    "Total",
   ];
 
+  const data = [
+    [
+      { value: 1 },
+      { value: "John Doe" },
+      ...Array.from({ length: 31 }, () => ({ value: Math.floor(Math.random() * 100) })),
+      { value: 0 },
+      { value: 0 },
+      { value: 0 },
+    ],
+  ];
 
   return (
-
-    <Spreadsheet data={data} columnLabels={columnLabels} />
-    
-  )
-}
+    <div className="spreadsheet-container">
+      <div className="scroll-wrapper">
+        <Spreadsheet data={data} columnLabels={columnLabels} />
+      </div>
+    </div>
+  );
+};
